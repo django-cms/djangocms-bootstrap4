0c265fde59ac8e0fffc8e4099cd87a6c6999007f
Angelo Dini <angelo.dini@divio.ch>
2015-12-11 11:26:35 +0100

    prepare release

--- aldryn_bootstrap3/models.py
+++ aldryn_bootstrap3/models.py
@@ -219,8 +219,8 @@ class Boostrap3ImagePlugin(CMSPlugin):
 
     file = filer.fields.image.FilerImageField(
         verbose_name=_("file"),
+        blank=False,
         null=True,
-        blank=True,
         on_delete=models.SET_NULL,
         related_name='+',
     )
@@ -745,7 +745,7 @@ class Bootstrap3CarouselPlugin(CMSPlugin):
 class Bootstrap3CarouselSlidePlugin(CMSPlugin, LinkMixin):
     image = filer.fields.image.FilerImageField(
         verbose_name=_('image'),
-        blank=True,
+        blank=False,
         null=True,
         related_name='+',
         on_delete=models.SET_NULL,
