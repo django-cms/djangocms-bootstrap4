963975e1d4143cd5b74c3172b8e996be74b8eadd
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2016-01-15 18:22:03 +0200

    Revert "Bootstrap3ImageCMSPlugin render(): remove useless context update() call"
    This reverts commit 6a0f658bce7c2aaf1f28d12757a166b8b1f1a321.

--- aldryn_bootstrap3/cms_plugins.py
+++ aldryn_bootstrap3/cms_plugins.py
@@ -234,6 +234,7 @@ class Bootstrap3ImageCMSPlugin(CMSPluginBase):
     )
 
     def render(self, context, instance, placeholder):
+        context.update({'instance': instance})
         if callable(filer_ajax_upload):
             # Use this in template to conditionally enable drag-n-drop.
             context.update({'has_dnd_support': True})
