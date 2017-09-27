6b6b2a7f8c8d35b38b10a70cd44b3b27edc9d7f0
Angelo Dini <angelo.dini@divio.ch>
2017-09-27 23:20:29 +0200

    added migrations (alpha), tests and locale

--- tests/tests_grid.py
+++ tests/tests_grid.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from djangocms_bootstrap4.contrib.bootstrap4_grid.models import (
     GRID_CONTAINERS,
     Bootstrap4GridContainer,
+    Bootstrap4GridRow,
 )
 
 
@@ -18,3 +19,16 @@ class Bootstrap4GridContainerPlugin(TestCase):
             container_type=GRID_CONTAINERS[0][0]
         )
         self.assertEqual(container.container_type, GRID_CONTAINERS[0][0])
+
+
+class Bootstrap4GridRowPlugin(TestCase):
+    def setUp(self):
+        Bootstrap4GridRow.objects.create(
+            pk=0,
+        )
+
+    def test_container_instance(self):
+        container = Bootstrap4GridRow.objects.get(
+            pk=0
+        )
+        self.assertEqual(container.pk, 0)
