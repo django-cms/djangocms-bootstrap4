01556dcf10dbe88937ec98eab2311b9049214147
Angelo Dini <angelo.dini@divio.ch>
2017-10-18 14:54:57 +0200

    Added picture plugin (#5)
    * added tests
    
    * fixed tests according to comments
    
    * added link plugin
    
    * fiex py3 and better naming
    
    * fiex py3 and better naming
    
    * wrap up the link plugin
    
    * docs fixes
    
    * migration fixes
    
    * added picture plugin
    
    * fixing link options
    
    * adaptions from comments
    
    * more simplification
    
    * changes
    
    * smaller adaptions
    
    * fix merge conflicts
    
    * added tests
    
    * added picture plugin
    
    * smaller adaptions
    
    * remove merge conflict
    
    * fixing fieldset assignment
    
    * remove redeclaration

---
+++ djangocms_bootstrap4/contrib/bootstrap4_picture/models.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from cms.models import CMSPlugin
+
+from djangocms_picture.models import AbstractPicture
+
+
+@python_2_unicode_compatible
+class Bootstrap4Picture(AbstractPicture):
+    """
+    Content > "Image" Plugin
+    https://getbootstrap.com/docs/4.0/content/images/
+    """
+    picture_fluid = models.BooleanField(
+        verbose_name=_('Responsive'),
+        default=True,
+        help_text=_('Adds the .img-fluid class to make the image responsive.'),
+    )
+    picture_rounded = models.BooleanField(
+        verbose_name=_('Rounded'),
+        default=False,
+        help_text=_('Adds the .rounded class for round corners.'),
+    )
+    picture_thumbnail = models.BooleanField(
+        verbose_name=_('Thumbnail'),
+        default=False,
+        help_text=_('Adds the .img-thumbnail class.'),
+    )
+
+    def __str__(self):
+        return str(self.pk)
--- tests/settings.py
+++ tests/settings.py
@@ -7,7 +7,12 @@ HELPER_SETTINGS = {
         'filer',
         'mptt',
         'djangocms_text_ckeditor',
+        'djangocms_link',
+        'djangocms_picture',
+        'djangocms_bootstrap4',
         'djangocms_bootstrap4.contrib.bootstrap4_grid',
+        'djangocms_bootstrap4.contrib.bootstrap4_link',
+        'djangocms_bootstrap4.contrib.bootstrap4_picture',
     ],
     'ALLOWED_HOSTS': ['localhost'],
     'CMS_LANGUAGES': {
