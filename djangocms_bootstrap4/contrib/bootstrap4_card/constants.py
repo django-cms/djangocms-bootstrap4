575e530f0738a62d14d2e3d2ea24a795394454aa
Angelo Dini <angelo.dini@divio.ch>
2017-12-19 15:19:37 +0100

    simplified cards system

--- djangocms_bootstrap4/contrib/bootstrap4_card/constants.py
+++ djangocms_bootstrap4/contrib/bootstrap4_card/constants.py
@@ -5,13 +5,6 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 
-CARD_BLUEPRINTS = (
-    ('', '-----'),
-    ('card', _('Card')), # image, body (title, text, button)
-    ('panel', _('Panel')), # header, body (title, text, button)
-    ('teaser', _('Teaser')), # body (title, text, button)
-)
-
 CARD_TYPES = (
     ('card', _('Card')),
     ('card-group', _('Card group')),
@@ -25,17 +18,10 @@ CARD_ALIGNMENT = (
     ('text-right', _('Right')),
 )
 
-CARD_INNER_TYPE = (
-    ('card-header', _('Header')),
+CARD_INNER_TYPES = (
     ('card-body', _('Body')),
+    ('card-header', _('Header')),
     ('card-footer', _('Footer')),
-)
-
-CARD_CONTENT_TYPE = (
-    ('card-text', _('Text')),
-    ('card-title', _('Title')),
-    ('card-subtitle', _('Subtitle')),
-    ('card-link', _('Link')),
 )
 
 CARD_TAG_CHOICES = (
@@ -48,10 +34,4 @@ CARD_TAG_CHOICES = (
     ('h6', 'H6'),
     ('p', 'P'),
     ('small', 'SMALL'),
-)
-
-CARD_IMAGE_TYPE = (
-    ('card-img-top', _('Image top')),
-    ('card-img-bottom', _('Image bottom')),
-    ('card-img-overlay', _('Image overlay')),
 )
