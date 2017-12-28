03acfa25affeb428d236abf28892a82863f4c444
Angelo Dini <angelo.dini@divio.ch>
2017-12-28 18:54:57 +0100

    Adds code and blockquote plugins (#23)
    * adds code and blockquote plugins
    
    * comment fixes

---
+++ djangocms_bootstrap4/contrib/bootstrap4_content/models.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from cms.models import CMSPlugin
+
+from djangocms_bootstrap4.fields import AttributesField
+
+from .constants import CODE_TYPE_CHOICES, QUOTE_ALIGN_CHOICES
+
+
+@python_2_unicode_compatible
+class Bootstrap4Code(CMSPlugin):
+    """
+    Content > "Code" Plugin
+    https://getbootstrap.com/docs/4.0/content/code/
+    """
+    code_content = models.TextField(
+        verbose_name=_('Code'),
+    )
+    tag_type = models.CharField(
+        verbose_name=_('Code type'),
+        choices=CODE_TYPE_CHOICES,
+        default=CODE_TYPE_CHOICES[0][0],
+        max_length=255,
+    )
+    attributes = AttributesField()
+
+    def __str__(self):
+        return str(self.pk)
+
+    def get_short_description(self):
+        return '<{}>'.format(self.tag_type)
+
+
+@python_2_unicode_compatible
+class Bootstrap4Blockquote(CMSPlugin):
+    """
+    Content > "Blockquote" Plugin
+    https://getbootstrap.com/docs/4.0/content/typography/#blockquotes
+    """
+    quote_content = models.TextField(
+        verbose_name=_('Quote'),
+    )
+    quote_origin = models.TextField(
+        verbose_name=_('Cite'),
+        blank=True,
+    )
+    quote_alignment = models.CharField(
+        verbose_name=_('Alignment'),
+        choices=QUOTE_ALIGN_CHOICES,
+        default=QUOTE_ALIGN_CHOICES[0][0],
+        blank=True,
+        max_length=255,
+    )
+    attributes = AttributesField()
+
+    def __str__(self):
+        return str(self.pk)
+
+    def get_short_description(self):
+        return self.quote_content
