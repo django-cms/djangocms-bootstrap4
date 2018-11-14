5b6bad44a75e785a1922b845ce5cd3c36eb21a37
Angelo Dini <angelo.dini@divio.ch>
2018-11-14 14:06:03 +0100

    small adaptions

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
