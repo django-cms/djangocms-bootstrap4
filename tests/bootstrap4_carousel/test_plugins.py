05922835a90d06602ab959244db6999a8fdc8970
Angelo Dini <angelo.dini@divio.ch>
2019-09-06 16:29:57 +0200

    revert changes

--- tests/bootstrap4_carousel/test_plugins.py
+++ tests/bootstrap4_carousel/test_plugins.py
@@ -32,31 +32,12 @@ class B4CarouselPluginTestCase(B4TestFixture, CMSTestCase):
         with self.login_user_context(self.superuser):
             response = self.client.get(self.request_url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(
-            response,
-            """<div class="carousel slide" data-interval="5000" data-keyboard="true"
-                data-pause="hover" data-ride="carousel" data-wrap="true" id="carousel-1">
-                <ol class="carousel-indicators">
-                </ol>
-                <div class="carousel-inner">
-                </div>
-                <a class="carousel-control-prev" data-slide="prev" href="#carousel-1" role="button">
-                    <span aria-hidden="true" class="carousel-control-prev-icon">
-                    </span>
-                    <span class="sr-only">
-                        Previous
-                    </span>
-                </a>
-                <a class="carousel-control-next" data-slide="next" href="#carousel-1" role="button">
-                    <span aria-hidden="true" class="carousel-control-next-icon">
-                    </span>
-                    <span class="sr-only">
-                        Next
-                    </span>
-                </a>
-            </div>""",
-            html=True,
-        )
+        self.assertContains(response, '<div class="carousel slide"')
+        self.assertContains(response, 'data-interval="5000"')
+        self.assertContains(response, 'data-keyboard="true"')
+        self.assertContains(response, 'data-pause="hover"')
+        self.assertContains(response, 'data-ride="carousel"')
+        self.assertContains(response, 'data-wrap="true"')
 
     def test_carousel_slide_plugin(self):
         row = add_plugin(
