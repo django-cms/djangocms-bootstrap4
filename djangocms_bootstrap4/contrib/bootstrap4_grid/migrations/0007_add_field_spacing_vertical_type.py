fb1dbf3268fc940b8f169847a8c9e5d863d210a1
Victor Yunenko <victor@what.digital>
2020-06-10 22:04:36 +0300

    grid: fix migrations

--- djangocms_bootstrap4/contrib/bootstrap4_grid/migrations/0007_add_field_spacing_vertical_type.py
+++ djangocms_bootstrap4/contrib/bootstrap4_grid/migrations/0007_add_field_spacing_vertical_type.py
@@ -2,8 +2,8 @@
 
 from django.db import migrations
 import djangocms_bootstrap4.contrib.bootstrap4_grid.models
+import djangocms_bootstrap4.contrib.bootstrap4_grid.constants
 import enumfields.fields
-import settings
 
 
 class Migration(migrations.Migration):
@@ -21,16 +21,16 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='bootstrap4gridcontainer',
             name='background',
-            field=enumfields.fields.EnumField(default='background-none', enum=settings.GridContainerBackground, max_length=255, verbose_name='Background'),
+            field=enumfields.fields.EnumField(default='background-none', enum=djangocms_bootstrap4.contrib.bootstrap4_grid.constants.GridContainerBackground, max_length=255, verbose_name='Background'),
         ),
         migrations.AlterField(
             model_name='bootstrap4gridcontainer',
             name='container_type',
-            field=enumfields.fields.EnumField(default='container', enum=settings.GridContainerType, max_length=255, verbose_name='Container width'),
+            field=enumfields.fields.EnumField(default='container', enum=djangocms_bootstrap4.contrib.bootstrap4_grid.constants.GridContainerType, max_length=255, verbose_name='Container width'),
         ),
         migrations.AlterField(
             model_name='bootstrap4gridcontainer',
             name='spacing',
-            field=enumfields.fields.EnumField(default='spacing-none', enum=settings.GridContainerSpacing, max_length=255, verbose_name='Vertical spacing'),
+            field=enumfields.fields.EnumField(default='spacing-none', enum=djangocms_bootstrap4.contrib.bootstrap4_grid.constants.GridContainerSpacing, max_length=255, verbose_name='Vertical spacing'),
         ),
     ]
