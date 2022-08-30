59af41ea8f9deb807f44394596977104558b6ead
Fabian Braun <fsbraun@gmx.de>
2022-08-30 15:40:35 +0200

    REmove apps.py

--- djangocms_bootstrap4/contrib/bootstrap4_badge/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.db import models
-
-
-class Bootstrap4Config(AppConfig):
-    name = 'bootstrap4_alerts'
-    verbose_name = 'Bootstrap 4 alerts'
-    default_auto_field = models.AutoField
