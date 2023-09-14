f78a1e339595f47dc39de1f4325b49f6bcde477a
Fabian Braun <fsbraun@gmx.de>
2023-09-14 20:45:08 +0200

    Update djangocms_bootstrap4/contrib/bootstrap4_grid/models.py

--- djangocms_bootstrap4/contrib/bootstrap4_grid/models.py
+++ djangocms_bootstrap4/contrib/bootstrap4_grid/models.py
@@ -3,7 +3,7 @@ from functools import partial
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from django.utils.translation import ngettext as ngettext
+from django.utils.translation import ngettext
 
 from cms.models import CMSPlugin
 
