148ec5d851b9610d94a2afd1ec9fc64302a34f89
Florian Repenn <florian.repenn@lfda.de>
2024-06-06 15:00:47 +0200

    container: replace gettext_lazy with gettext

--- djangocms_bootstrap4/contrib/bootstrap4_grid/models.py
+++ djangocms_bootstrap4/contrib/bootstrap4_grid/models.py
@@ -1,7 +1,7 @@
 from functools import partial
 
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 from django.utils.translation import ngettext
 
